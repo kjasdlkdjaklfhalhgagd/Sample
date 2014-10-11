76a03a905f1215316a8178808704a1d4a99e5c08
kurosofi <kurosofi.19931218@gmail.com>
2014-10-11 16:06:40 +0900

    no message

---
+++ php/簡易掲示板/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCUTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<titile>掲示板</titile>
+</head>
+<body>
+	<h1>掲示板</h1>
+	<form>
+		message:<input type="text"
+	</form>
+	<h2>投稿一覧(0件)</h2>
+</body>
+</html>
